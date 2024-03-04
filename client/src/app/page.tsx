c347bc0923d9dabae37893ce01416a8dd2321655
Felicia <ffeliciachang@gmail.com>
2024-03-04 17:50:10 -0500

    wip

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,12 +3,11 @@ import HomeContainer from "./components/Home";
 import { init } from "@jamsocket/javascript/server";
 
 const spawnBackend = init({
-  account: "felicia",
-  service: "stability-demo",
+  account: "ffeliciachang",
+  service: "renaissance-earth",
   // NOTE: we want to keep the Jamsocket token secret, so we can only do this in a server component
   // We'll leave this blank for now, since we don't need it when developing with the dev CLI
-  token: "",
-  apiUrl: process.env.JAMSOCKET ?? "http://localhost:8080",
+  token: process.env.JAMSOCKET ?? '',
 });
 
 export default async function Page() {
